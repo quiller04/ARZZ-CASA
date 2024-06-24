30b7e509def84241a5c72ef30c280ae762dd9eef
Guilherme <guilhermequillerdealencar@gmail.com>
2024-06-24 15:02:03 -0300

    arquivo antigo principal (server) fica de beckup

--- server.js
+++ server.js
@@ -3,9 +3,61 @@ const loginInfo = require('./login');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
 
-  const checkPermissionDenied = async () => {
+  const loginCasaArezzo = async (page) => {
+    console.log('Acessando a página de login...');
+    await page.goto('https://casa.arezzo.com.br/login.php?cdlingua=');
+
+    console.log('Esperando o campo de usuário ficar visível...');
+    await page.waitForSelector('#cdusuario', { visible: true });
+
+    console.log('Limpando e preenchendo o campo de usuário...');
+    await page.focus('#cdusuario');
+    await page.keyboard.down('Control');
+    await page.keyboard.press('A');
+    await page.keyboard.up('Control');
+    await page.keyboard.press('Backspace');
+    await page.type('#cdusuario', loginInfo.arezzo.username);
+
+    console.log('Esperando o campo de senha ficar visível...');
+    await page.waitForSelector('#cdsenha', { visible: true });
+
+    console.log('Habilitando e preenchendo o campo de senha...');
+    await page.evaluate(() => {
+      document.querySelector('#cdsenha').removeAttribute('disabled');
+    });
+    await page.focus('#cdsenha');
+    await page.keyboard.down('Control');
+    await page.keyboard.press('A');
+    await page.keyboard.up('Control');
+    await page.keyboard.press('Backspace');
+    await page.type('#cdsenha', loginInfo.arezzo.password);
+
+    console.log('Submetendo o formulário de login...');
+    await Promise.all([
+      page.click('#btnLogin'),
+      page.waitForNavigation(),
+    ]);
+
+    console.log('Login realizado com sucesso!');
+  };
+
+  const loginNDDPrint = async (loginPage) => {
+    console.log('Acessando a página de login do NDD Print...');
+    await loginPage.goto('https://360.nddprint.com/login/company');
+    await loginPage.waitForSelector('#login-company-input-company', { visible: true });
+
+    console.log('Preenchendo os dados de login do NDD Print...');
+    await loginPage.type('#login-company-input-company', loginInfo.nddPrint.company);
+    await loginPage.type('#login-company-input-logon', loginInfo.nddPrint.email);
+    await loginPage.type('#login-company-input-password', loginInfo.nddPrint.password);
+    await loginPage.click('#login-button-submit');
+    await loginPage.waitForNavigation();
+
+    console.log('Login no NDD Print realizado com sucesso!');
+  };
+
+  const checkPermissionDenied = async (page) => {
     const permissionDeniedMessage = await page.evaluate(() => {
       const div = document.querySelector('div[style="font: bold 10px verdana, arial, sens-serif; background: #F2F2D9; border: 1px solid #DFDF9F; padding: 3px 5px;"]');
       return div ? div.textContent.trim() : null;
@@ -13,7 +65,7 @@ const loginInfo = require('./login');
     return permissionDeniedMessage !== null && permissionDeniedMessage.includes('Permissão negada');
   };
 
-  const checkForSpecificPhrase = async () => {
+  const checkForSpecificPhrase = async (page) => {
     const inputText = await page.evaluate(() => {
       const input = document.querySelector('#nmtitulochamado');
       return input ? input.value.trim() : null;
@@ -21,7 +73,7 @@ const loginInfo = require('./login');
     return inputText === 'Solicitar PIN de Impressão';
   };
 
-  const getFullNameAndCCID = async () => {
+  const getFullNameAndCCID = async (page) => {
     const result = await page.evaluate(() => {
       const textarea = document.querySelector('#dschamado');
       const content = textarea ? textarea.textContent.trim() : '';
@@ -36,60 +88,15 @@ const loginInfo = require('./login');
     return result;
   };
 
-  console.log('Acessando a página de login...');
-  await page.goto('https://casa.arezzo.com.br/login.php?cdlingua=');
-
-  console.log('Esperando o campo de usuário ficar visível...');
-  await page.waitForSelector('#cdusuario', { visible: true });
-
-  console.log('Limpando e preenchendo o campo de usuário...');
-  await page.focus('#cdusuario');
-  await page.keyboard.down('Control');
-  await page.keyboard.press('A');
-  await page.keyboard.up('Control');
-  await page.keyboard.press('Backspace');
-  await page.type('#cdusuario', loginInfo.arezzo.username);
-
-  console.log('Esperando o campo de senha ficar visível...');
-  await page.waitForSelector('#cdsenha', { visible: true });
-
-  console.log('Habilitando e preenchendo o campo de senha...');
-  await page.evaluate(() => {
-    document.querySelector('#cdsenha').removeAttribute('disabled');
-  });
-  await page.focus('#cdsenha');
-  await page.keyboard.down('Control');
-  await page.keyboard.press('A');
-  await page.keyboard.up('Control');
-  await page.keyboard.press('Backspace');
-  await page.type('#cdsenha', loginInfo.arezzo.password);
-
-  console.log('Submetendo o formulário de login...');
-  await Promise.all([
-    page.click('#btnLogin'),
-    page.waitForNavigation(),
-  ]);
-
-  console.log('Login realizado com sucesso!');
+  const page = await browser.newPage();
+  await loginCasaArezzo(page);
 
   const loginPage = await browser.newPage();
-  console.log('Acessando a página de login do NDD Print...');
-  await loginPage.goto('https://360.nddprint.com/login/company');
-  await loginPage.waitForSelector('#login-company-input-company', { visible: true });
+  await loginNDDPrint(loginPage);
 
-  console.log('Preenchendo os dados de login do NDD Print...');
-  await loginPage.type('#login-company-input-company', loginInfo.nddPrint.company);
-  await loginPage.type('#login-company-input-logon', loginInfo.nddPrint.email);
-  await loginPage.type('#login-company-input-password', loginInfo.nddPrint.password);
-  await loginPage.click('#login-button-submit');
-  await loginPage.waitForNavigation();
-
-  console.log('Login no NDD Print realizado com sucesso!');
-
-  let currentNumber = 600610;
+  let currentNumber = 602635;
   let lastVisitedURL = '';
 
-  
   while (true) {
     await page.bringToFront();
     const nextURL = `https://casa.arezzo.com.br/html/hd/hdchamado/cadastro_chamado.php?cdchamado=${currentNumber}`;
@@ -97,9 +104,9 @@ const loginInfo = require('./login');
 
     await page.goto(nextURL);
 
-    const hasSpecificPhrase = await checkForSpecificPhrase();
+    const hasSpecificPhrase = await checkForSpecificPhrase(page);
     if (hasSpecificPhrase) {
-      const { fullName, ccid } = await getFullNameAndCCID();
+      const { fullName, ccid } = await getFullNameAndCCID(page);
       console.log(`Frase específica encontrada. Nome completo do usuário: ${fullName}, CCID: ${ccid}`);
 
       await loginPage.bringToFront();
@@ -199,7 +206,7 @@ const loginInfo = require('./login');
       await new Promise(resolve => setTimeout(resolve, 1000));
       if (existingPin && existingPin.length > 0) {
         console.log(`PIN existente encontrado: ${existingPin}`);
-      //} else {
+        //} else {
         console.log('Gerando novo PIN...');
         await new Promise(resolve => setTimeout(resolve, 1000));
         await loginPage.evaluate(() => {
@@ -212,63 +219,28 @@ const loginInfo = require('./login');
         const modalText = await loginPage.evaluate(() => {
           const modal = document.querySelector('.ndd-ng-dialog__body.container');
           if (modal) {
-            const message = modal.querySelector('.ndd-ng-dialog__info__message span');
-            if (message && message.innerText.includes('Não é possível gerar e enviar código PIN, pois o usuário não possui um e-mail cadastrado.')) {
-              return true;
-            }
+            return modal.textContent.trim();
           }
-          return false;
+          return null;
         });
-        if (modalText) {
-          console.log('Modal de atenção encontrado. Simulando Enter.');
-          await loginPage.keyboard.press('Enter');
-          await new Promise(resolve => setTimeout(resolve, 1000));
-          await loginPage.evaluate(() => {
-            const generatePinButton = document.querySelector('#userauth-btn-generateandsend-pin');
-            if (generatePinButton) {
-              generatePinButton.click();
-            }
-          });
-        }
 
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await loginPage.keyboard.press('Tab');
-        await loginPage.keyboard.type('5');
-        await loginPage.keyboard.press('Enter');
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await loginPage.reload();
-        await loginPage.waitForSelector('.ndd-ng-data-item__value', { visible: true });
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        existingPin = await loginPage.evaluate(() => {
+        console.log(`Texto do modal: ${modalText}`);
+        await loginPage.evaluate(() => {
+          const confirmButton = document.querySelector('.ndd-ng-button.ndd-ng-dialog__button-action');
+          if (confirmButton) {
+            confirmButton.click();
+          }
+        });
+
+        await new Promise(resolve => setTimeout(resolve, 10000));
+
+        const updatedPin = await loginPage.evaluate(() => {
           const pinElement = document.querySelector('.ndd-ng-data-item__value');
-          return pinElement ? pinElement.textContent.trim() : null;
+          return pinElement ? pinElement.innerText.trim() : null;
         });
 
-        if (existingPin && existingPin.length > 0) {
-          console.log(`Novo PIN gerado: ${existingPin}`);
-        }
+        console.log(`PIN gerado: ${updatedPin}`);
       }
-
-      await page.bringToFront();
-      await page.click('#XMLTababa_atendimento');
-      await page.waitForSelector('#btnStart', { visible: true });
-      await page.click('#btnStart');
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      await page.waitForSelector('#dsacompanhamento', { visible: true });
-      await page.type('#dsacompanhamento', 'PIN encaminhado para o e-mail cadastrado, por favor esperar aproximadamente 30 minutos para replicar em nosso sistema');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('ArrowDown');
-      await page.click('#btnClose');
-      await new Promise(resolve => setTimeout(resolve, 3000));
-      console.log('PIN encaminhado e chamado encerrado.');
-
-      lastVisitedURL = nextURL;
-    }
-
-    const permissionDenied = await checkPermissionDenied();
-    if (permissionDenied) {
-      console.log(`Permissão negada na URL ${nextURL}. Tentando novamente...`);
-      continue;
     }
 
     currentNumber++;
